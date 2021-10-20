8a9b8d20fbb7ecab203ad9b67234652f5c785d29
vinicius <vinicius@houpa.app>
2021-10-19 22:17:46 -0300

    feat: route of login and callback of github created

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,18 @@
+import "dotenv/config";
 import express from 'express';
 
 const app = express();
 
+app.get('/github', (req, res) => {
+    res.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`);
+});
+
+app.get('/signin/callback', (req, res) => {
+    const {code} = req.query;
+
+    res.json(code);
+});
+
 app.listen(4000, () => {
     console.log('Server is running on port 4000');
-    });
+});
